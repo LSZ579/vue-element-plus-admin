b16309b3d620969b70747df872b156a22cccb501
lusz <122720267@qq.com>
2021-12-16 14:53:26 +0800

    fix: fix: 修改配置

--- dist/static/js/dashboard.8a34ce3b.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["dashboard"],{b30e:function(e,n,c){"use strict";c.r(n);var a=c("7a23"),o={class:"app-container"};function t(e,n){return Object(a["openBlock"])(),Object(a["createElementBlock"])("div",o," 首页 ")}var r=c("6b0d"),s=c.n(r);const d={},p=s()(d,[["render",t]]);n["default"]=p}}]);
